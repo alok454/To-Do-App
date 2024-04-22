d05be6aab8dce46df2a4dbe9a408d41b661d6c9b
Alok Kumar <alokkumar82980@gmail.com>
2024-04-22 15:40:21 +0530

    features add after feedback

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const input = document.querySelector("#input");
 const submit = input.nextElementSibling;
 const todoList = document.querySelector(".todo-list");
 
-console.log(document.createElement("i"))
 
 submit.addEventListener("click", (e) => {
     let inputText = input.value;
@@ -14,35 +13,60 @@ submit.addEventListener("click", (e) => {
 })
 
 function addTask(inputText) {
-    const div = document.createElement("div");
+    const taskDiv = document.createElement("div");
     const listItem = document.createElement("span");
     const innerDiv = document.createElement("div")
     const editButton = document.createElement("span");
     const deleteButton = document.createElement("span");
 
-    listItem.textContent = inputText;
-    
+    listItem.textContent = inputText.trim();
+
     editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
     deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
 
-    div.appendChild(listItem);
-    div.appendChild(innerDiv);
+    taskDiv.appendChild(listItem);
+    taskDiv.appendChild(innerDiv);
 
     innerDiv.appendChild(editButton);
     innerDiv.appendChild(deleteButton);
 
-    todoList.appendChild(div);
+    todoList.appendChild(taskDiv);
 
-    listItem.addEventListener("click", () => {
+    
+    editButton.addEventListener("click", () => {      /* for edit task */
+        listItem.classList.add("hide-task");
+        editButton.classList.add("hide-edit-sym");
+
+        const editBox = document.createElement("div");
+        const saveButton = document.createElement("button");
+        const editInput = document.createElement("input");
+        
+        editInput.value = listItem.textContent;  // add task text into input field
+        editBox.classList.add("edit-box")
+        editInput.classList.add("edit-input");
+        saveButton.classList.add("edit-button");
+
+        saveButton.textContent = "Edit";
+        editBox.appendChild(editInput);
+        editBox.appendChild(saveButton);
+        taskDiv.prepend(editBox);
+        editInput.focus();
+
+        saveButton.addEventListener("click", (e) => {      /* for save task after edit */
+            if(editInput.value.trim()) {
+                listItem.textContent = editInput.value.trim();
+                listItem.classList.remove("hide-task");
+                editButton.classList.remove("hide-edit-sym");
+                editBox.remove();
+            }
+        }, false)
+    }, false)
+
+    deleteButton.addEventListener("click", () => {    /* for delete task */
+        deleteButton.parentElement.parentElement.remove();
+    }, false)
+
+    listItem.addEventListener("click", () => {       /* for completed task */
         listItem.classList.toggle("completed");
-    })
-
-    editButton.addEventListener("click", () => {
-        input.value = listItem.textContent
-        editButton.parentElement.parentElement.remove()
-    },false)
-
-    deleteButton.addEventListener("click", () => {
-        deleteButton.parentElement.parentElement.remove()
-    },false)
+    }, false)
 }
